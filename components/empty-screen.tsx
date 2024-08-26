03f157f63343311d02de6b9863bc5a077a4ed83f
“Nancy <nvigil08@gmail.com>
2024-08-26 10:23:55 -0700

    empty screen text

--- components/empty-screen.tsx
+++ components/empty-screen.tsx
@@ -9,10 +9,12 @@ export function EmptyScreen() {
     <div className="mx-auto max-w-2xl px-4">
       <div className="flex flex-col gap-2 rounded-lg border bg-background p-8">
         <h1 className="text-lg font-semibold">
-          Welcome to Next.js AI Chatbot!
+          How difficult is it to get AI to do what you want?
         </h1>
         <p className="leading-normal text-muted-foreground">
-          This is an open source AI chatbot app template built with{' '}
+          This is an AI chatbot app that will help you understand the importance of
+          aligning AI to human values.
+
           <ExternalLink href="https://nextjs.org">Next.js</ExternalLink>, the{' '}
           <ExternalLink href="https://sdk.vercel.ai">
             Vercel AI SDK
@@ -24,13 +26,8 @@ export function EmptyScreen() {
           .
         </p>
         <p className="leading-normal text-muted-foreground">
-          It uses{' '}
-          <ExternalLink href="https://vercel.com/blog/ai-sdk-3-generative-ui">
-            React Server Components
-          </ExternalLink>{' '}
-          to combine text with generative UI as output of the LLM. The UI state
-          is synced through the SDK so the model is aware of your interactions
-          as they happen.
+          Here we will explore the concepts of goal misgeneralization, goal Misspecification,
+          and specification gaming.
         </p>
       </div>
     </div>
