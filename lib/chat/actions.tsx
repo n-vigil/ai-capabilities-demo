12a623636f4bd45f44e8804b12ee86e991d9bcd2
“Nancy <nvigil08@gmail.com>
2024-08-25 17:43:32 -0700

    add dependencies

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -10,6 +10,31 @@ import {
 } from 'ai/rsc'
 import { Anthropic } from '@anthropic-ai/sdk'
 
+import Anthropic from "@anthropic-ai/sdk";
+
+const anthropic = new Anthropic();
+
+const msg = await anthropic.messages.create({
+model: "claude-3-5-sonnet-20240620",
+max_tokens: 1000,
+temperature: 0,
+system: "Respond only with short poems.",
+messages: [
+    {
+    "role": "user",
+    "content": [
+        {
+        "type": "text",
+        "text": "Why is the ocean salty?"
+        }
+    ]
+    }
+]
+});
+console.log(msg);
+
+
+
 import {
   spinner,
   BotCard,
