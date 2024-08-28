cc1ba4b26b7c526abac0c609bac374f40b65a7c7
“Nancy <nvigil08@gmail.com>
2024-08-28 15:39:10 -0700

    padding

--- lib/chat/actions.tsx
+++ lib/chat/actions.tsx
@@ -8,31 +8,7 @@ import {
   streamUI,
   createStreamableValue
 } from 'ai/rsc'
-import { Anthropic } from '@anthropic-ai/sdk'
 import { openai } from '@ai-sdk/openai'
-
-// import Anthropic from "@anthropic-ai/sdk";
-
-// const anthropic = new Anthropic();
-
-// const msg = await anthropic.messages.create({
-// model: "claude-3-5-sonnet-20240620",
-// max_tokens: 1000,
-// temperature: 0,
-// system: "Respond only with short poems.",
-// messages: [
-//     {
-//     "role": "user",
-//     "content": [
-//         {
-//         "type": "text",
-//         "text": "Why is the ocean salty?"
-//         }
-//     ]
-//     }
-// ]
-// });
-// console.log(msg);
 
 
 
